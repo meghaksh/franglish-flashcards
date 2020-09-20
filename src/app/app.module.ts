fc8c5ce964d9d93845fdedc5a64af5b5a3967739
Meghaksh Brahmbhatt <meghaksh4footprints@gmail.com>
2020-09-20 10:37:44 +0530

    Post Submit / View functionality
    Model, Service, Subject, Observer, EventEmitter(Replaced by Subject), Angular Material For UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './posts/header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    //ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit{
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post'},
+  //   {title : 'Second Post', content: 'This is the seond post'},
+  //   {title : 'Third Post', content: 'This is the third post'},
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postService: PostsService){
+
+
+  }
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postsSub = this.postService.getPostUpdateListner()
+    .subscribe((posts: Post[])=>{
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
+  }
+}
